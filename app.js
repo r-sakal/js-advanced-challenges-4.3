98e8f21886497e9c714485a01b963fb693d536ce
Rob <rob.sakal23@gmail.com>
2025-07-19 00:41:01 -0400

    question 1

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('it worked')
+//Q1 Show Rating 
+//      Give  a rating, display a star (*) for each full rating
+//       and a full-stop (.) for each half rating.
+
+//showRating(3)
+//showRating(4.5)
+//showRating(0.5)
