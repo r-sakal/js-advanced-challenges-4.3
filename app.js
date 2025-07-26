c5fe8a0d26fc0377e241db724342f1336054a083
Rob <rob.sakal23@gmail.com>
2025-07-25 22:40:30 -0400

    Q1 answered

--- app.js
+++ app.js
@@ -5,3 +5,23 @@
 //showRating(3)
 //showRating(4.5)
 //showRating(0.5)
+
+function showRating(rating) {
+let ratings = "";
+for (let i = 0; i < Math.floor(rating); ++i) {
+    //ratings = ratings + "*"
+    //shorten this part by using +=
+    ratings += "*"
+    if (i !== Math.floor(rating - 1)) {
+        //ratings = ratings + " ";
+        ratings += " "
+    }
+}
+if (!Number.isInteger(rating)) {
+    //ratings = ratings + " .";
+    ratings += " ."
+}
+return ratings;
+}
+
+console.log(showRating(2.5))
